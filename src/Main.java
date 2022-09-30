1689e5ffc31a236ef9446f1412751a08f7f1a5c6
Armando Medina <armsves@gmail.com>
2022-09-30 15:38:12 +0200

    mejoras casi listo falta pruebas y revisar los party0 y party1

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,3 @@ public class Main {
             }
         }
 }
-
-//2 parties de 5 personajes cada uno.
-//Verificación de existencia de party, si existe sobreescribir o volver al menú anterior.
-//1. Creación manual de jugadorya
-//Estas en party 1 ya
-//Crear jugardor 1 ya
-//tipo de jugador enum MAGO o GUERRERO ya
-//si MAGO -> createWizard pida los datos de wizard nombre, salud, att1(mana), att2(int)
-//si Guerrero -> createWarrior pida los datos de guerrero nombre, salud, att1(streght), att2(stamina)
-//2. Creación aleatoria de jugador ya
--- src/Menu.java
+++ src/Menu.java
@@ -254,6 +254,5 @@ public class Menu {
         //Music.startStopBattleMusic();
     }
 
-
 }
 
