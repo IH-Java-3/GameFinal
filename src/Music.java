a95ef2a7cbf39552462c0e29490194939cc442de
Armando Medina <armsves@gmail.com>
2022-10-03 08:42:33 +0200

    Versión final

--- src/Music.java
+++ src/Music.java
@@ -1,10 +1,6 @@
 public class Music {
     static Thread t = new Thread(() -> Sound.playSound("c:/java/game/src/main.wav"), "PlaySound");
-
-    public static void startMusic() {
-        t.start();
-    }
-
+    public static void startMusic() { t.start(); }
     public static void stopMusic() {
         if (t.isAlive()) t.stop();
     }
